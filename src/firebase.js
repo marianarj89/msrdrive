ffd21a6db1744c3a279e0bfc38b46369855f6099
Mariana Ribas <mariana.rj89@gmail.com>
2024-03-16 19:22:36 -0300

    update2

--- src/firebase.js
+++ src/firebase.js
@@ -2,33 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-let firebaseConfig = {};
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_APIKEY,
+  authDomain: import.meta.env.FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.FIREBASE_APPID
+};
 
-if (import.meta.env.MODE === "development") {
-  // Use development keys
-  firebaseConfig = {
-    apiKey: "YOUR_DEV_API_KEY",
-    authDomain: "YOUR_DEV_AUTH_DOMAIN",
-    projectId: "YOUR_DEV_PROJECT_ID",
-    storageBucket: "YOUR_DEV_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_DEV_MESSAGING_SENDER_ID",
-    appId: "YOUR_DEV_APP_ID"
-  };
-} else {
-  // Use .env variables
-  firebaseConfig = {
-    apiKey: process.env.FIREBASE_APIKEY,
-    authDomain: process.env.FIREBASE_AUTHDOMAIN,
-    projectId: process.env.FIREBASE_PROJECTID,
-    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.FIREBASE_APPID
-  };
-}
+console.log("Firebase Configuration:", firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth(app); 
+
+console.log("Firebase App initialized:", app);
+console.log("Firebase Auth initialized:", auth);
 
 export { auth }; 
 export default app;
